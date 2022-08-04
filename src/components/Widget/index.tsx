c77b74d586f7274706094f6e98b510a253e87bc4
Julia Frederico <magalhaesjulia@id.uff.br>
2022-08-04 14:16:32 -0300

    adds bottom sheet to widget component

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useRef } from "react";
 import { TouchableOpacity } from 'react-native';
 import { ChatTeardropDots } from 'phosphor-react-native';
+import BottomSheet from "@gorhom/bottom-sheet";
 
 import { styles } from './styles';
 import { theme } from "../../theme";
 
 export function Widget() {
+    const bottomSheetRef = useRef<BottomSheet>(null);
+
     return (
         <>
             <TouchableOpacity
@@ -16,6 +19,13 @@ export function Widget() {
                     color={theme.colors.text_on_brand_color}
                 />
             </TouchableOpacity>
+
+            <BottomSheet
+                ref={bottomSheetRef}
+                snapPoints={[1, 280]}
+            >
+
+            </BottomSheet>
         </>
     );
 };
