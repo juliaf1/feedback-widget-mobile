f74d509f9cf5b1a6a9266895e85912d2d15172e6
Julia Frederico <magalhaesjulia@id.uff.br>
2022-08-04 14:37:30 -0300

    adds copyright component content and styling

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -4,9 +4,10 @@ import { ChatTeardropDots } from 'phosphor-react-native';
 import BottomSheet from '@gorhom/bottom-sheet';
 import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 
+import { Options } from '../Options';
+
 import { styles } from './styles';
 import { theme } from '../../theme';
-import { Options } from '../Options';
 
 function Widget() {
     const bottomSheetRef = useRef<BottomSheet>(null);
