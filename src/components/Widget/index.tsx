606e26d60bb4eef9225eb826913463360f71f95b
Julia Frederico <magalhaesjulia@id.uff.br>
2022-08-04 14:27:09 -0300

    adds styling to modal background and indicator

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -29,6 +29,8 @@ function Widget() {
             <BottomSheet
                 ref={bottomSheetRef}
                 snapPoints={[1, 280]}
+                backgroundStyle={styles.modal}
+                handleIndicatorStyle={styles.indicator}
             >
 
             </BottomSheet>
