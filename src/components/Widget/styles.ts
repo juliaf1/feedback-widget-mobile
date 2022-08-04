46b62e765aa75ffe4b2f5c94a605962e6fc2df2d
Julia Frederico <magalhaesjulia@id.uff.br>
2022-08-04 13:56:58 -0300

    adds chat button to styles

--- src/components/Widget/styles.ts
+++ src/components/Widget/styles.ts
@@ -1,7 +1,16 @@
 import { StyleSheet } from "react-native";
+import { theme } from "../../theme";
 
 export const styles = StyleSheet.create({
-    container: {
-        
+    button: {
+        width: 48,
+        height: 48,
+        borderRadius: 24,
+        backgroundColor: theme.colors.brand,
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+        right: 16,
+        bottom: 16
     }
 });
