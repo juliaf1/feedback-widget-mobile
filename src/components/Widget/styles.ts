3ef998244f2620794412a3befc671c9e4e6916f9
Julia Frederico <magalhaesjulia@id.uff.br>
2022-08-04 13:58:36 -0300

    updates chat button bottom distance to use getBottomSpace from iphone helper

--- src/components/Widget/styles.ts
+++ src/components/Widget/styles.ts
@@ -1,4 +1,6 @@
 import { StyleSheet } from "react-native";
+import { getBottomSpace } from "react-native-iphone-x-helper";
+
 import { theme } from "../../theme";
 
 export const styles = StyleSheet.create({
@@ -11,6 +13,6 @@ export const styles = StyleSheet.create({
         alignItems: 'center',
         position: 'absolute',
         right: 16,
-        bottom: 16
+        bottom: getBottomSpace() + 16
     }
 });
