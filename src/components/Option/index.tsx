31c2eb5b825bc77c97d0bed34b90353d677d888e
Julia Frederico <magalhaesjulia@id.uff.br>
2022-08-04 14:55:31 -0300

    adds component layout content and styling

--- src/components/Option/index.tsx
+++ src/components/Option/index.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
-import { View } from 'react-native';
+import {
+    TouchableOpacity,
+    TouchableOpacityProps,
+    Image,
+    ImageProps,
+    Text
+} from 'react-native';
 
 import { styles } from './styles';
 
-export function Option() {
+interface Props extends TouchableOpacityProps {
+    title: string;
+    image: ImageProps;
+};
+
+export function Option({title, image, ...rest}: Props) {
     return(
-        <View style={styles.container}>
-        </View>
+        <TouchableOpacity
+            style={styles.container}
+            {...rest}
+        >
+            <Image
+                source={image}
+                style={styles.image}
+            />
+            <Text style={styles.title}>
+                {title}
+            </Text>
+        </TouchableOpacity>
     )
 };
