52418a184b9daf59f66b7adce8d28fb0aa2e2998
Julia Frederico <magalhaesjulia@id.uff.br>
2022-08-04 14:33:29 -0300

    create options component file structure and imports it to widget component

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -1,11 +1,12 @@
-import React, { useRef } from "react";
+import React, { useRef } from 'react';
 import { TouchableOpacity } from 'react-native';
 import { ChatTeardropDots } from 'phosphor-react-native';
-import BottomSheet from "@gorhom/bottom-sheet";
+import BottomSheet from '@gorhom/bottom-sheet';
 import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 
 import { styles } from './styles';
-import { theme } from "../../theme";
+import { theme } from '../../theme';
+import { Options } from '../Options';
 
 function Widget() {
     const bottomSheetRef = useRef<BottomSheet>(null);
@@ -32,6 +33,7 @@ function Widget() {
                 backgroundStyle={styles.modal}
                 handleIndicatorStyle={styles.indicator}
             >
+                <Options />
 
             </BottomSheet>
         </>
