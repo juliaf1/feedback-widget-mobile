3f96e563c38965f11c496c65f8100a3e294aa300
Julia Frederico <magalhaesjulia@id.uff.br>
2022-08-08 13:21:28 -0300

    adds title to feedback widget component

--- src/components/Options/index.tsx
+++ src/components/Options/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 
 import { Copyright } from '../Copyright';
 import { Option } from '../Option';
@@ -10,6 +10,10 @@ import { styles } from './styles';
 export function Options() {
     return(
         <View style={styles.container}>
+            <Text style={styles.title}>
+                Deixe seu feedback
+            </Text>
+
             <View style={styles.options}>
                 {
                     Object
